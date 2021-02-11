3def92f96534b489933fd2d55f8708af15528b32
YongheeAn <ayh0429@gmail.com>
2021-02-11 23:55:26 +0900

    Set Vue & Firebase

---
+++ vf/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyA6gLmOWRrs332oUqMprpFy8vc5oVklU00',
+  authDomain: 'vueandfirebase-b43f2.firebaseapp.com',
+  databaseURL: 'https://vueandfirebase-b43f2-default-rtdb.firebaseio.com',
+  projectId: 'vueandfirebase-b43f2',
+  storageBucket: 'vueandfirebase-b43f2.appspot.com',
+  messagingSenderId: '301851016707',
+  appId: '1:301851016707:web:f12a7f60853faa3e6e1836'
+}
